5f9c50cbdd2c4f2581044f4e6f4ba75795cac049
Zihang Tang <eden@Zihangs-MacBook-Pro.local>
2023-07-03 19:05:46 +0800

    fix: update response format and error handling in token price API

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 BASE_URL = "https://api.binance.com"
 
 price_cache = {}
-price_cache["USDT"] = 0
+price_cache["USDT"] = {"price": 1, "priceChangePercent": 0, "success": True}
 
 last_sync_timestamps = {}
 
@@ -27,19 +27,23 @@ def sync_token_price_data(symbols):
     response = binance_api_request("/api/v3/ticker/24hr", params={"symbols": str(symbols).replace("'", '"').replace(' ', '')})
 
     if 'code' in response:
-        return
+        return response
 
     for item in response:
         symbol = str(item["symbol"]).replace('USDT', '')
+        price = float(item["lastPrice"])
         price_change_percent = float(item["priceChangePercent"])
-        price_cache[symbol] = price_change_percent
+        price_cache[symbol] = {"price": price, "priceChangePercent": price_change_percent, "success": True}
         last_sync_timestamps[symbol] = time.time()
+
+    return None
 
 def get_token_price_change(tokens):
     global last_sync_timestamps, price_cache
 
     current_timestamp = time.time()
-    result = {}
+    result = []
+    error = None
 
     query_list=[]
 
@@ -47,35 +51,39 @@ def get_token_price_change(tokens):
         last_sync_timestamp = last_sync_timestamps.get(token)
 
         if last_sync_timestamp is None:
-            sync_token_price_data([token])
+            error = sync_token_price_data([token])
         elif (current_timestamp - last_sync_timestamp) > sync_interval:
             query_list.append(token)
 
-    sync_token_price_data(query_list)
+    if query_list:
+        error = sync_token_price_data(query_list)
+
+    if error:
+        return [], error
 
     for token in tokens:
-        price_change_percent = price_cache.get(token)
+        price_data = price_cache.get(token, {"success": False})
 
-        if price_change_percent is None:
-            result[token] = "Token not found."
-        else:
-            result[token] = price_change_percent
+        result.append({token: price_data})
 
-    return result
+    return result, None
 
 @app.route("/", methods=["POST"])
 def api():
     data = request.get_json()
 
     if "tokens" not in data:
-        return jsonify({"error": "Tokens parameter is missing."}), 400
+        return jsonify({"code": 400, "message": "Tokens parameter is missing.", "data": [], "success": False}), 400
 
     print(data["tokens"])
 
     tokens = data["tokens"]
-    result = get_token_price_change(tokens)
+    result_data, error = get_token_price_change(tokens)
 
-    return jsonify(result)
+    if error:
+        return jsonify({"code": error["code"], "message": error["msg"], "data": [], "success": False}), 400
+
+    return jsonify({"code": 0, "message": "ok", "data": result_data, "success": True})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=12002)
